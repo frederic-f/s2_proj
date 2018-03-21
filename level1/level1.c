954fc9c111762873705f785e51a7bab7b3bac288
= <pv8@me.com>
2018-03-21 16:57:55 +0100

    level1 terminé

--- level1/level1.c
+++ level1/level1.c
@@ -3,23 +3,15 @@
 /* Size of the window */
 #define SCREEN_WIDTH  720
 #define SCREEN_HEIGHT 540
-/* Size of the frame picture */
-#define FRAME_WIDTH   720
-#define FRAME_HEIGHT  540
 
-/* In the sprite, we have 8 images of a 32x32 picture,
- * 2 images for each direction. */
+
+/* In the sprite, we have 45 images of a 187x157 picture */
+
 /* Size of one image: */
-#define SPRITE_SIZE     32
+#define SPRITE_WIDTH     187
+#define SPRITE_HEIGHT    157
 
-/* Order of the different directions in the picture: */
-#define DIR_UP          0
-#define DIR_RIGHT       1
-#define DIR_DOWN        2
-#define DIR_LEFT        3
 
-/* Number of pixels for one step of the sprite */
-#define SPRITE_STEP     5
 
 /* Handle events coming from the user:
         - quit the game?
@@ -29,7 +21,7 @@
    We also change the animation bit used for creating the "walk" effect.
    */
 void HandleEvent(SDL_Event event,
-        int *quit, int *currDirection, int *animFlip, SDL_Rect *position)
+        int *quit, int *currOrientation)
 {
     switch (event.type) {
         /* close button clicked */
@@ -45,25 +37,18 @@ void HandleEvent(SDL_Event event,
                     *quit = 1;
                     break;
                 case SDLK_LEFT:
-                    *currDirection = DIR_LEFT;
-                    *animFlip = 1 - *animFlip;
-                    position->x -= SPRITE_STEP;
+                    // launcher rotates to the left, unless already at extreme left
+                    if (*currOrientation > 0) {
+                        *currOrientation -= 1 ;
+                    }
                     break;
                 case SDLK_RIGHT:
-                    *currDirection = DIR_RIGHT;
-                    *animFlip = 1 - *animFlip;
-                    position->x += SPRITE_STEP;
+                    // launcher rotates to the right, unless already at far right
+                    if (*currOrientation < 44) {
+                        *currOrientation += 1 ;
+                    }
                     break;
-                case SDLK_UP:
-                    *currDirection = DIR_UP;
-                    *animFlip = 1 - *animFlip;
-                    position->y -= SPRITE_STEP;
-                    break;
-                case SDLK_DOWN:
-                    *currDirection = DIR_DOWN;
-                    *animFlip = 1 - *animFlip;
-                    position->y += SPRITE_STEP;
-                    break;
+
                 default:
                     break;
             }
@@ -73,20 +58,12 @@ void HandleEvent(SDL_Event event,
 
 int main(int argc, char* argv[])
 {
-    SDL_Surface *screen, *temp, *sprite, *frame;
+    SDL_Surface *screen, *temp, *sprite, *frame ;
+
     int colorkey;
 
-    /* Information about the current situation of the sprite: */
-    int currentDirection = DIR_RIGHT;
-    /* Walking in a direction is animated with 2 images; we use a boolean
-     * that we flip after each movement to show them in turn and create
-     * the illusion */
-    int animationFlip = 0;
-
-    /* Rectangle to store the position of the sprite in the window.
-     * Only the x and y coordinates are used. */
-    SDL_Rect spritePosition;
-
+    /* Information about the current situation of the launcher sprite */
+    int currentOrientation = 22 ; // 0 = pointing left, 22 = upwards, 45 = right
 
     /* initialize SDL */
     SDL_Init(SDL_INIT_VIDEO);
@@ -100,23 +77,42 @@ int main(int argc, char* argv[])
     /* set keyboard repeat */
     SDL_EnableKeyRepeat(10, 10);
 
-
-
-
     /* load frame */
     temp  = SDL_LoadBMP("frame_1p.bmp");
     frame = SDL_DisplayFormat(temp);
     SDL_FreeSurface(temp);
 
 
-    /* load arrow */
+    /* load sprite (launcher) */
     temp = SDL_LoadBMP("frame_launcher.bmp");
-    launcher = SDL_DisplayFormat(temp) ;
+    sprite = SDL_DisplayFormat(temp) ;
     SDL_FreeSurface(temp) ;
 
+    /* creation of a cache to hide the launcher sprite */
+
+    SDL_Rect cache ;
+    cache.x = 0 ;
+    cache.y = 0 ;
+    cache.w = SCREEN_WIDTH ;
+    cache.h = SCREEN_HEIGHT ;
+
+    int color = SDL_MapRGB(screen->format, 0, 0, 0) ;
+
+
+    /* setup SPRITE colorkey and turn on RLE */
+    colorkey = SDL_MapRGB(screen->format, 255, 0, 255);
+    SDL_SetColorKey(sprite, SDL_SRCCOLORKEY | SDL_RLEACCEL, colorkey);
+
+
+    /* setup FRAME colorkey and turn on RLE */
+    SDL_SetColorKey(frame, SDL_SRCCOLORKEY | SDL_RLEACCEL, colorkey);
+
+    /* Rectangle to store the position of the sprite in the window. */
+    SDL_Rect spritePosition;
+
     /* set sprite position in the bottoom of the window */
-    spritePosition.x = (SCREEN_WIDTH - SPRITE_SIZE) / 2;
-    spritePosition.y = (SCREEN_HEIGHT - SPRITE_SIZE) / 2;
+    spritePosition.x = (SCREEN_WIDTH - SPRITE_WIDTH) / 2; // in the middle of the window in WIDTH
+    spritePosition.y = SCREEN_HEIGHT - SPRITE_HEIGHT ; // at the bottom in HEIGHT
     
     int gameover = 0;
 
@@ -128,29 +124,31 @@ int main(int argc, char* argv[])
         /* look for an event; possibly update the position and the shape
          * of the sprite. */
         if (SDL_PollEvent(&event)) {
-            HandleEvent(event, &gameover, &currentDirection,
-                    &animationFlip, &spritePosition);
+            HandleEvent(event, &gameover, &currentOrientation);
         }
 
-        /* collide with edges of screen 
-        if (spritePosition.x <= 0)
-            spritePosition.x = 0;
-        if (spritePosition.x >= SCREEN_WIDTH - SPRITE_SIZE) 
-            spritePosition.x = SCREEN_WIDTH - SPRITE_SIZE;
-
-        if (spritePosition.y <= 0)
-            spritePosition.y = 0;
-        if (spritePosition.y >= SCREEN_HEIGHT - SPRITE_SIZE) 
-	spritePosition.y = SCREEN_HEIGHT - SPRITE_SIZE;*/
-
-	
-	// draw the frame
-	SDL_Rect position;
-	position.x = 0 ;
-	position.y = 0 ;
-	SDL_BlitSurface(frame, NULL, screen, &position) ;
+        // draw the cache
+        SDL_FillRect(screen, &cache, color) ;
 
 
+	    // draw the frame
+        // first a Rect for the position
+	    SDL_Rect framePosition;
+        // then set the position
+        framePosition.x = 0 ;
+        framePosition.y = 0 ;
+        // then draw on screen
+	    SDL_BlitSurface(frame, NULL, screen, &framePosition) ;
+
+
+        // draw the sprit
+        SDL_Rect spriteImage ;
+        spriteImage.w = SPRITE_WIDTH ;
+        spriteImage.h = SPRITE_HEIGHT ;
+        spriteImage.x = 0 ; // the image is moved in height, not in width
+        spriteImage.y = SPRITE_HEIGHT * currentOrientation ;
+
+        SDL_BlitSurface(sprite, &spriteImage, screen, &spritePosition) ;
 
 
 
@@ -160,7 +158,7 @@ int main(int argc, char* argv[])
     }
 
     /* clean up */
-
+    SDL_FreeSurface(sprite) ;
     SDL_FreeSurface(frame);
     SDL_Quit();
 
